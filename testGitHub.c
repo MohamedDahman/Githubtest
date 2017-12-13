9c803726d0833b365920063f18a706ec8705ae57
Mohamed Dahman <32441101+MohamedDahman@users.noreply.github.com>
2017-12-13 14:46:47 +0100

    Add files via upload

---
+++ testGitHub.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+void printtext(string text1);
+void printtext(string text1, int value);
+
+
+
+int main(void)
+{
+        printtext("the value");
+        printtext("the second value",2);
+
+}
+
+
+void printtext(string text1);
+{
+// to do
+
+}
+
+void printtext(string text1, int value)
+{
+//to do
+}
